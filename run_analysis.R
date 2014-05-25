a62fbff8f5677853bc860fe942e24d2383d728e9
Pablo Fleurquin <pablofleurquin@gmail.com>
2014-05-25 19:51:36 -0400

    Fourth Commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,9 @@
 # Loading Data
 setwd("~/Desktop/Coursera/DataScience/02GettingCleaningData/CourseProject01")
-Xtest<-read.table("X_test.txt")
+'Xtest<-read.table("X_test.txt")
 Ytest<-read.table("y_test.txt")
 Xtrain<-read.table("X_train.txt")
-Ytrain<-read.table("y_train.txt")
+Ytrain<-read.table("y_train.txt")'
 VarNames0<-as.vector(t(read.table("features.txt")[2]))
 
 # Cleaning Variables names
@@ -20,5 +20,10 @@ Test<-cbind(Xtest,Ytest)
 Train<-cbind(Xtrain,Ytrain)
 CompleteDataset<-rbind(Test,Train)
 
+# Create a dataset with the mean for each measurment for each subject
+MeanDataset<-aggregate(CompleteDataset[,1:length(CompleteDataset)-1],
+                 by=list(CompleteDataset$subject), mean)
+
 # Write Complete file to CSV
-write.table(CompleteDataset,file="CompleteData.csv",sep=",",row.names=FALSE)
+write.table(CompleteDataset,file="CompleteData.txt",sep=",",row.names=FALSE)
+write.table(MeanDataset,file="SamsungMeanData.txt",sep=",",row.names=FALSE)
