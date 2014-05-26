e9c0b37b2ddda5271cdac6616acfccabb73b19c3
Pablo Fleurquin <pablofleurquin@gmail.com>
2014-05-25 20:27:53 -0400

    Last Commit

--- run_analysis.R
+++ run_analysis.R
@@ -4,25 +4,33 @@ Xtest<-read.table("X_test.txt")
 Ytest<-read.table("y_test.txt")
 Xtrain<-read.table("X_train.txt")
 Ytrain<-read.table("y_train.txt")
+SubjectTest<-read.table("subject_test.txt")
+SubjectTrain<-read.table("subject_train.txt")
 VarNames0<-as.vector(t(read.table("features.txt")[2]))
 
 # Cleaning Variables names
 VarNames<-tolower(gsub("\\(|\\)|,|-|_","", VarNames0))
 
-# Giving data a header
-names(Xtest)   <-VarNames
-names(Xtrain)  <-VarNames
-names(Ytest)   <-"subject"
-names(Ytrain)  <-"subject"
+# Giving data a header and descriptive activity names
+names(Xtest)        <-VarNames
+names(Xtrain)       <-VarNames
+names(Ytest)        <-"activity"
+names(Ytrain)       <-"activity"
+names(SubjectTrain) <-"subject"
+names(SubjectTest)  <-"subject"
+factor(Ytest,levels=c(1,2,3,4,5,6),
+       labels=c("walking","walking_up","walinkg_down","sitting","standing","laying"))
+factor(Ytrain,levels=c(1,2,3,4,5,6),
+       labels=c("walking","walking_up","walinkg_down","sitting","standing","laying"))
 
 # Binding data
-Test<-cbind(Xtest,Ytest)
-Train<-cbind(Xtrain,Ytrain)
+Test<-cbind(SubjectTest,Ytest,Xtest)
+Train<-cbind(SubjectTrain,Ytrain,Xtrain)
 CompleteDataset<-rbind(Test,Train)
 
 # Create a dataset with the mean for each measurment for each subject
-MeanDataset<-aggregate(CompleteDataset[,1:length(CompleteDataset)-1],
-                 by=list(CompleteDataset$subject), mean)
+MeanDataset<-aggregate(CompleteDataset[,2:length(CompleteDataset)],
+                 by=list(CompleteDataset$subject,CompleteDataset$activity), mean)
 
 # Write Complete file to CSV
 write.table(CompleteDataset,file="CompleteData.txt",sep=",",row.names=FALSE)
